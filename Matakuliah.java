a21455b41cd6c18f472e15fcde8089e96c84a123
tugas <aldayanuar6@gmail.com>
2025-02-27 19:59:51 +0700

    percobaan 3

--- Matakuliah.java
+++ Matakuliah.java
@@ -1,13 +1,13 @@
 public class Matakuliah {
     String kode;
     String nama;
-    int sks, jumlahJam;
+    int sks;
 
     public Matakuliah(String kode, String nama, int sks) {
         this.kode = kode;
         this.nama = nama;
         this.sks = sks;
-        this.jumlahJam = jumlahJam;
+       
     }
 
     void cetakInfo() {
